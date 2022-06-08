52537ad1b63cc6a6c70273d1d7f4170029ae3af4
Fernanda Romao <Fernanda Romao>
2022-06-08 09:49:12 -0300

    finaliza requisito 2

--- src/ContextProvider.js
+++ src/ContextProvider.js
@@ -4,18 +4,28 @@ import Context from './Context';
 
 function ContextProvider({ children }) {
   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [filterByName, setFilterByName] = useState({ name: '' });
+
   useEffect(() => {
     const fetchPlanets = async () => {
       const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
       const dataPlanets = await response.json();
       setData(dataPlanets.results);
+      setFilteredData(dataPlanets.results);
     };
     fetchPlanets();
   }, []);
 
+  useEffect(() => {
+    const filterPlanet = data
+      .filter((planet) => planet.name.toLowerCase().includes(filterByName));
+    setFilteredData(filterPlanet);
+  }, [filterByName]);
+
   return (
     <main>
-      <Context.Provider value={ { data } }>
+      <Context.Provider value={ { filteredData, setFilterByName } }>
         { children }
       </Context.Provider>
     </main>
